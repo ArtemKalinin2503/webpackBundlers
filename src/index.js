ed7254e84f5f8d5c37f751a4fe847febcae1c3c1
kalinin <kalinin@inventive.ru>
2019-05-13 21:47:46 +0300

    сборка на webpack 4

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './modules/slider.index'; //Экспорт кода из файла modules/slider.index.js
+import './styles/css/index.css'; //Подключаем стили css
+import './styles/less/index.less'; //Подключаем стили less
+import './styles/sass/index.sass'; //Подключаем стили sass
---
+++ src/modules/slider.index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery'; //Подключаем JQ
+
+var test = "hello";
+console.log(test);
+
+var header = $('header');
+// header.html('тест подключения JQ...')
