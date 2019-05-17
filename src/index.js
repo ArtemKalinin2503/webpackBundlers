e54f835dc2748ca0a092e35b74e2ab9597b75792
kalinin <kalinin@inventive.ru>
2019-05-17 14:43:30 +0300

    добавил поддрежку jq для плагинов типа OWL

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './modules/slider.index'; //Экспорт кода из файла modules/slider.index.js
+import './modules/main'; //Экспорт кода из файла modules/main.js
 import './styles/css/index.css'; //Подключаем стили css
 import './styles/less/index.less'; //Подключаем стили less
 import './styles/sass/index.sass'; //Подключаем стили sass
--- webpack.config.js
+++ webpack.config.js
@@ -6,77 +6,82 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  optimization: {
-    minimizer: [new OptimizeCssAssetsPlugin({}), new UglifyJsPlugin({})]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    port: 8000
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/index.pug'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.less$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
-      },
-      {
-        test: /\.sass$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-        options: {
-          pretty: true
-        }
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'images/[name].[ext]'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(otf|eot|woff|ttf|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'fonts/[name].[ext]'
-            }
-          }
-        ]
-      }
-    ]
-  }
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	optimization: {
+		minimizer: [ new OptimizeCssAssetsPlugin({}), new UglifyJsPlugin({}) ]
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, 'dist'),
+		port: 8000
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.pug'
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'style.css'
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			'window.jQuery': 'jquery'
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+			},
+			{
+				test: /\.less$/,
+				use: [ MiniCssExtractPlugin.loader, 'css-loader', 'less-loader' ]
+			},
+			{
+				test: /\.sass$/,
+				use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.pug$/,
+				loader: 'pug-loader',
+				options: {
+					pretty: true
+				}
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'images/[name].[ext]'
+						}
+					}
+				]
+			},
+			{
+				test: /\.(otf|eot|woff|ttf|woff2)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'fonts/[name].[ext]'
+						}
+					}
+				]
+			}
+		]
+	}
 };
 
 //Запуск приложения npm run dev/npm run dev:watch/npm run build/npm run start (запуск сервера)
